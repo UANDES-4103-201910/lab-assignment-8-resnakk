4527e5138140c0352fdc9ad79cf3044b6a2e1ae9
resnakk <masanchez3@miuandes.cl>
2019-05-09 23:57:37 -0400

    merecido 2

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,26 @@
-// Note: $(() => {}); is equivalent to $(document).ready(function(){})
-$(() => {
-	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+
+$(function(){
+	var $write = $('#write');
+
+	$(' .key').click(function(){
+		
+		var $this  = $(this),
+		character = $this.html();
+
+	
+	if ($this.hasClass('key key-space')) character = ' ';
+	if ($this.hasClass('tab')) character = "\t";
+	if ($this.hasClass('key key-special key-wide')) character = "\n";
+	
+			
+	if ($this.hasClass('key key-wide')) {
+		var html = $write.html();
+	
+		$write.html(html.substr(0, html.length - 1));
+		return false;
+		}
+
+	$write.html($write.html() + character);
+			
+			});
 });
